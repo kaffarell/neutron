fdb192bb32111eccb57dbad6c6a5b31deb34af86
gabriel <gabrielgoller123@gmail.com>
2019-03-14 21:15:55 +0100

    changed from richtextbox to fastcolored textbox better syntax highlithing, line number and other features

--- neutron/Form1.Designer.cs
+++ neutron/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -41,11 +42,13 @@
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.neutronToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.documentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.updateInBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.updateWithGitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -129,7 +132,6 @@
             this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.neutronToolStripMenuItem,
             this.documentToolStripMenuItem,
-            this.updateInBrowserToolStripMenuItem,
             this.updateWithGitToolStripMenuItem});
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
@@ -138,39 +140,23 @@
             // neutronToolStripMenuItem
             // 
             this.neutronToolStripMenuItem.Name = "neutronToolStripMenuItem";
-            this.neutronToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
+            this.neutronToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
             this.neutronToolStripMenuItem.Text = "neutron";
             this.neutronToolStripMenuItem.Click += new System.EventHandler(this.neutronToolStripMenuItem_Click);
             // 
             // documentToolStripMenuItem
             // 
             this.documentToolStripMenuItem.Name = "documentToolStripMenuItem";
-            this.documentToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
+            this.documentToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
             this.documentToolStripMenuItem.Text = "Document";
             this.documentToolStripMenuItem.Click += new System.EventHandler(this.documentToolStripMenuItem_Click);
-            // 
-            // updateInBrowserToolStripMenuItem
-            // 
-            this.updateInBrowserToolStripMenuItem.Name = "updateInBrowserToolStripMenuItem";
-            this.updateInBrowserToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
-            this.updateInBrowserToolStripMenuItem.Text = "update in browser";
-            this.updateInBrowserToolStripMenuItem.Click += new System.EventHandler(this.updateInBrowserToolStripMenuItem_Click);
             // 
             // updateWithGitToolStripMenuItem
             // 
             this.updateWithGitToolStripMenuItem.Name = "updateWithGitToolStripMenuItem";
-            this.updateWithGitToolStripMenuItem.Size = new System.Drawing.Size(209, 26);
+            this.updateWithGitToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
             this.updateWithGitToolStripMenuItem.Text = "update with git";
             this.updateWithGitToolStripMenuItem.Click += new System.EventHandler(this.updateWithGitToolStripMenuItem_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(11, 34);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(679, 431);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // label1
             // 
@@ -180,14 +166,56 @@
             this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 2;
             // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;]+);\r\n^\\s*(case|default)\\s*[^:]" +
+    "*(?<range>:)\\s*(?<range>[^;]+);\r\n";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(31, 17);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.fastColoredTextBox1.CharHeight = 17;
+            this.fastColoredTextBox1.CharWidth = 10;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.fastColoredTextBox1.LeftBracket = '(';
+            this.fastColoredTextBox1.LeftBracket2 = '{';
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(12, 31);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.RightBracket = ')';
+            this.fastColoredTextBox1.RightBracket2 = '}';
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(676, 435);
+            this.fastColoredTextBox1.TabIndex = 3;
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(700, 478);
+            this.Controls.Add(this.fastColoredTextBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Product Sans", 8.13913F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -197,6 +225,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,7 +237,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.Label label1;
@@ -217,9 +246,10 @@
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem documentToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem updateInBrowserToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem updateWithGitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem neutronToolStripMenuItem;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
     }
 }
 
--- neutron/Form1.cs
+++ neutron/Form1.cs
@@ -10,10 +10,9 @@ using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Net;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
 using System.Diagnostics;
-
+using IWshRuntimeLibrary;
+using ScintillaNET;
 
 namespace notepad
 {
@@ -22,118 +21,115 @@ namespace notepad
         public Form1()
         {
             InitializeComponent();
-            richTextBox1.ShortcutsEnabled = true;
-            richTextBox1.Font = new Font("Product Sans", 10);
-            richTextBox1.Dock = DockStyle.Fill;
-            richTextBox1.AcceptsTab = true;
+            fastColoredTextBox1.Font = new Font("Product Sans", 10);
+            fastColoredTextBox1.Dock = DockStyle.Fill;
+            fastColoredTextBox1.AcceptsTab = true;
         }
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
             string path3 = @AppDomain.CurrentDomain.BaseDirectory + "first_time_shortcut.txt";
-            string text = File.ReadAllText(path3);
-            if (File.Exists(path3) && (text != "0"))
+            string text = System.IO.File.ReadAllText(path3);
+            if (System.IO.File.Exists(path3) && (text != "0"))
             {
-                string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ;
+                string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 string linkName = "neutron";
+                string batDir = string.Format(@AppDomain.CurrentDomain.BaseDirectory);
 
-                using (StreamWriter writer = new StreamWriter(deskDir + "\\" + linkName + ".url"))
-                {
-                    string app = System.Reflection.Assembly.GetExecutingAssembly().Location;
-                     writer.WriteLine("[InternetShortcut]");
-                     writer.WriteLine("URL=file:///" + app);
-                     writer.WriteLine("IconIndex=0");
-                     string icon = app.Replace('\\', '/');
-                     writer.WriteLine("IconFile=" + icon);
-                     writer.Flush();
-                }
+                WshShell shell = new WshShell();
+                IWshShortcut link = (IWshShortcut)shell.CreateShortcut(deskDir + "\\" + linkName + ".lnk");
+                link.TargetPath = batDir + "\\neutron.exe";
+                link.Save();
+
                 text = "0";
-                File.WriteAllText(path3, text);
+                System.IO.File.WriteAllText(path3, text);
+
             }
+
+
+        }
+
+        private void fastColoredTextBox1_TextChanged(object sender, EventArgs e)
+        {
             
+        //    this.CheckKeyword("while", Color.OrangeRed, 0);
+        //    this.CheckKeyword("true", Color.OrangeRed, 0);
+        //    this.CheckKeyword("false", Color.OrangeRed, 0);
+        //    this.CheckKeyword("for", Color.OrangeRed, 0);
+        //    this.CheckKeyword("if", Color.DarkOrange, 0);
+        //    this.CheckKeyword("echo", Color.DarkOrange, 0);
+        //    this.CheckKeyword("pause", Color.DarkOrange, 0);
+        //    this.CheckKeyword("@echo", Color.DarkOrange, 0);
+        //    this.CheckKeyword("nul", Color.DarkOrange, 0);
+        //    this.CheckKeyword("rmdir", Color.DarkOrange, 0);
+        //    this.CheckKeyword("cd", Color.DarkOrange, 0);
+        //    this.CheckKeyword("else", Color.DarkOrange, 0);
+        //    this.CheckKeyword("int", Color.Blue, 0);
+        //    this.CheckKeyword("string", Color.Blue, 0);
+        //    this.CheckKeyword("double", Color.Blue, 0);
+        //    this.CheckKeyword("var", Color.Blue, 0);
+        //    this.CheckKeyword("null", Color.DarkBlue, 0);
 
+        //    string text = fastColoredTextBox1.Text;
+        //    foreach (var line in fastColoredTextBox1.Lines)
+        //    {
+        //        if (line.Contains("#"))
+        //        {
+        //            int firstcharindex = fastColoredTextBox1.GetFirstCharIndexOfCurrentLine();
+
+        //            int currentline = fastColoredTextBox1.GetLineFromCharIndex(firstcharindex);
+
+        //            fastColoredTextBox1.Select(firstcharindex, 10);
+
+        //            fastColoredTextBox1.SelectionColor = Color.Red;
+
+        //            fastColoredTextBox1.DeselectAll();
+        //            fastColoredTextBox1.Select(fastColoredTextBox1.Text.Length, 0);
+        //        }else if (line.Contains("//"))
+        //        {
+        //            int firstcharindex = fastColoredTextBox1.GetFirstCharIndexOfCurrentLine();
+
+        //            int currentline = fastColoredTextBox1.GetLineFromCharIndex(firstcharindex);
+
+        //            fastColoredTextBox1.Select(firstcharindex, 10);
+
+        //            fastColoredTextBox1.SelectionColor = Color.Green;
+
+        //            fastColoredTextBox1.DeselectAll();
+        //            fastColoredTextBox1.Select(fastColoredTextBox1.Text.Length, 0);
+        //        }
+        //        else
+        //        {
+        //            int firstcharindex = fastColoredTextBox1.GetFirstCharIndexOfCurrentLine();
+
+        //            int currentline = fastColoredTextBox1.GetLineFromCharIndex(firstcharindex);
+
+        //            fastColoredTextBox1.Select(firstcharindex, 10);
+
+        //            fastColoredTextBox1.SelectionColor = Color.Black;
+
+        //            fastColoredTextBox1.DeselectAll();
+        //            fastColoredTextBox1.Select(fastColoredTextBox1.Text.Length, 0);
+        //        }
+        //    }
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-            this.CheckKeyword("while", Color.OrangeRed, 0);
-            this.CheckKeyword("true", Color.OrangeRed, 0);
-            this.CheckKeyword("false", Color.OrangeRed, 0);
-            this.CheckKeyword("for", Color.OrangeRed, 0);
-            this.CheckKeyword("if", Color.DarkOrange, 0);
-            this.CheckKeyword("echo", Color.DarkOrange, 0);
-            this.CheckKeyword("pause", Color.DarkOrange, 0);
-            this.CheckKeyword("@echo", Color.DarkOrange, 0);
-            this.CheckKeyword("nul", Color.DarkOrange, 0);
-            this.CheckKeyword("rmdir", Color.DarkOrange, 0);
-            this.CheckKeyword("cd", Color.DarkOrange, 0);
-            this.CheckKeyword("else", Color.DarkOrange, 0);
-            this.CheckKeyword("int", Color.Blue, 0);
-            this.CheckKeyword("string", Color.Blue, 0);
-            this.CheckKeyword("double", Color.Blue, 0);
-            this.CheckKeyword("var", Color.Blue, 0);
-            this.CheckKeyword("null", Color.DarkBlue, 0);
+        //private void CheckKeyword(string word, Color color, int startIndex)
+        //{
+        //    if (this.fastColoredTextBox1.Text.Contains(word))
+        //    {
+        //        int index = -1;
+        //        int selectStart = this.fastColoredTextBox1.SelectionStart;
 
-            string text = richTextBox1.Text;
-            foreach (var line in richTextBox1.Lines)
-            {
-                if (line.Contains("#"))
-                {
-                    int firstcharindex = richTextBox1.GetFirstCharIndexOfCurrentLine();
-
-                    int currentline = richTextBox1.GetLineFromCharIndex(firstcharindex);
-
-                    richTextBox1.Select(firstcharindex, 10);
-
-                    richTextBox1.SelectionColor = Color.Red;
-
-                    richTextBox1.DeselectAll();
-                    richTextBox1.Select(richTextBox1.Text.Length, 0);
-                }else if (line.Contains("//"))
-                {
-                    int firstcharindex = richTextBox1.GetFirstCharIndexOfCurrentLine();
-
-                    int currentline = richTextBox1.GetLineFromCharIndex(firstcharindex);
-
-                    richTextBox1.Select(firstcharindex, 10);
-
-                    richTextBox1.SelectionColor = Color.Green;
-
-                    richTextBox1.DeselectAll();
-                    richTextBox1.Select(richTextBox1.Text.Length, 0);
-                }
-                else
-                {
-                    int firstcharindex = richTextBox1.GetFirstCharIndexOfCurrentLine();
-
-                    int currentline = richTextBox1.GetLineFromCharIndex(firstcharindex);
-
-                    richTextBox1.Select(firstcharindex, 10);
-
-                    richTextBox1.SelectionColor = Color.Black;
-
-                    richTextBox1.DeselectAll();
-                    richTextBox1.Select(richTextBox1.Text.Length, 0);
-                }
-            }
-        }
-
-        private void CheckKeyword(string word, Color color, int startIndex)
-        {
-            if (this.richTextBox1.Text.Contains(word))
-            {
-                int index = -1;
-                int selectStart = this.richTextBox1.SelectionStart;
-
-                while ((index = this.richTextBox1.Text.IndexOf(word, (index + 1))) != -1)
-                {
-                    this.richTextBox1.Select((index + startIndex), word.Length);
-                    this.richTextBox1.SelectionColor = color;
-                    this.richTextBox1.Select(selectStart, 0);
-                    this.richTextBox1.SelectionColor = Color.Black;
-                }
-            }
-        }
+        //        while ((index = this.fastColoredTextBox1.Text.IndexOf(word, (index + 1))) != -1)
+        //        {
+        //            this.fastColoredTextBox1.Select((index + startIndex), word.Length);
+        //            this.fastColoredTextBox1.SelectionColor = color;
+        //            this.fastColoredTextBox1.Select(selectStart, 0);
+        //            this.fastColoredTextBox1.SelectionColor = Color.Black;
+        //        }
+        //    }
+        //}
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -147,7 +143,7 @@ namespace notepad
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                richTextBox1.Text = File.ReadAllText(openFileDialog.FileName);
+                fastColoredTextBox1.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
                 directory = openFileDialog.InitialDirectory + openFileDialog.FileName;
                 name_file = openFileDialog.FileName;
             }
@@ -187,7 +183,7 @@ namespace notepad
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = "";
+            fastColoredTextBox1.Text = "";
             saveasFile();
         }
 
@@ -195,7 +191,7 @@ namespace notepad
         {
             var path = directory;
             StreamWriter writer = new StreamWriter(path);
-            string content = richTextBox1.Text;         
+            string content = fastColoredTextBox1.Text;         
 
             writer.Write(content);
             writer.Close();
@@ -211,7 +207,7 @@ namespace notepad
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                File.WriteAllText(saveFileDialog.FileName, richTextBox1.Text);
+                System.IO.File.WriteAllText(saveFileDialog.FileName, fastColoredTextBox1.Text);
                 directory = saveFileDialog.InitialDirectory + saveFileDialog.FileName;
                 name_file = saveFileDialog.FileName;
             }
@@ -225,7 +221,7 @@ namespace notepad
             FontDialog fd = new FontDialog();
             if (fd.ShowDialog() == DialogResult.OK)
             {
-                richTextBox1.Font = fd.Font;
+                fastColoredTextBox1.Font = fd.Font;
             }
         }
 
@@ -250,33 +246,18 @@ namespace notepad
 
         public string name_file { get; set; }
 
-        private void updateInBrowserToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            string downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ;
-
-
-
-            var parameters = new FirefoxOptions();
-            parameters.AddArgument("--headless");
-
-            FirefoxOptions options = new FirefoxOptions();
-            options.SetPreference("browser.download.folderList", 2);
-            options.SetPreference("browser.download.manager.showWhenStarting", false);
-            options.SetPreference("browser.download.dir", downloadPath);
-            options.SetPreference("browser.helperApps.neverAsk.saveToDisk", "kaffarell/neuron/archive/master.zip");
-
-            IWebDriver browser = new FirefoxDriver(options);
-
-            browser.Navigate().GoToUrl("https://github.com/kaffarell/neuron/archive/master.zip");
-
-
-
-        }
 
         private void updateWithGitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Process proc = null;
             string batDir = string.Format(@AppDomain.CurrentDomain.BaseDirectory);
+                
+            string path = batDir + "\\first_time_shortcut.txt";
+            StreamWriter writer = new StreamWriter(path);
+            string content = "1";
+            writer.Write(content);
+            writer.Close();
+
             proc = new Process();
             proc.StartInfo.WorkingDirectory = batDir;
             proc.StartInfo.FileName = "update.bat";
